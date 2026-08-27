efd7494972e23f0f37d5b81c095bb1e112394f8d
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-08-27 10:42:07 -0300

    feat: created blank space form test

--- web/cypress/e2e/formulario.cy.js
+++ web/cypress/e2e/formulario.cy.js
@@ -4,7 +4,7 @@ describe('formulario', () => {
     cy.contains('button', 'Formulário').should('be.visible').click()
     cy.contains('h1', 'Consultoria').should('be.visible')
 
-    cy.get('#name').type('Fiama')
+    cy.get('#name').type('Fiama ama')
     cy.get('#email').type('Fiama@fiama.fiama')
     cy.get('#phone').type('11 99999-9999')
     cy.get('#consultancyType').select('In Company')
@@ -27,7 +27,29 @@ describe('formulario', () => {
 
     const techs = ['Python, ', 'Java, ', 'JavaScript, ', 'HTML, ', 'CSS']
     techs.forEach(tech => {
-      cy.get('#technologies').type(tech)
+      cy.get('#technologies').type(tech + '{enter}').should('be.visible')
     })
+    cy.get("input[type='file']").selectFile('./cypress/fixtures/image.jpg' , {force: true})
+
+    cy.contains('label', 'Mais Detalhes').type('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, amet animi delectus esse eum fugit, harum laborum minima molestiae numquam obcaecati odio omnis optio qui quos saepe veniam voluptate, voluptates!').should('be.visible')
+
+    cy.contains('label', 'Li e aceito os termos de uso').find('input[type=checkbox]').click().should('be.checked')
+
+    cy.contains('button', 'Enviar formulário').click()
+
+    cy.contains("Sucesso").should('be.visible')
+  })
+  it('blank spaces', () => {
+    cy.login('4DM@gmail.com', '4DM')
+    cy.contains('button', 'Formulário').should('be.visible').click()
+    cy.contains('h1', 'Consultoria').should('be.visible')
+
+
+
+    cy.contains('button', 'Enviar formulário').click()
+
+    cy.contains('Digite nome e sobrenome').should('be.visible')
+    cy.contains('Informe um email válido').should('be.visible')
+    cy.contains('Você precisa aceitar os termos de uso').should('be.visible')
   })
 })
